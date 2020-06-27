e0c18bd05ffcc64f48b56e5f7aa115840782d87e
Atul Kumar Shaw <atul.shaw@optimizeitsystems.com>
2020-06-27 13:59:04 +0530

    Initial Commit

---
+++ Assesment2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assesment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter input");
+            List<string> lst = new List<string>();
+            while (true)
+            {
+                string userinput = Console.ReadLine();
+                if (userinput.Contains("#"))
+                {
+                    lst.Add(userinput);
+                }
+                if (userinput.ToUpper() == "THE END")
+                {
+                    for (int i = 0; i < lst.Count; i++)
+                    {
+                        if (i > 1)
+                        {
+                            if (lst[i].Split('#')[1].Trim() == lst[i - 1].Split('#')[1].Trim())
+                            {
+                                Console.WriteLine(Convert.ToString(lst[i - 1]).Replace("#"," "));
+                                break;
+                            }
+                        }
+                    }                    
+                }                
+            }
+        }
+    }
+}
---
+++ Assignment1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter input ");
+            while (true)
+            {
+                double s1 = 0; double s2 = 0; double s3 = 0;               
+                string val = Console.ReadLine();
+                string[] valarr = val.Split(' ');
+                s1 = Convert.ToDouble(valarr[0]); s2 = Convert.ToDouble(valarr[1]); s3 = Convert.ToDouble(valarr[2]);
+                if (s2 > s1 && (s3 < s1 || s3 < s2))
+                {
+                    Console.WriteLine("right-angled");
+                }
+                else if (s2 == s3 && (s1 < s2 || s1 < s3))
+                {
+                    Console.WriteLine("isosceles");
+                }
+                else if (s1 == s2 && s2 == s3)
+                {
+                    Console.WriteLine("equilateral");
+                }
+                else if (s1 == s2 && s3 > s2)
+                {
+                    Console.WriteLine("invalid");
+                }
+                else
+                {
+                    Console.WriteLine("notspecial");
+                }
+               
+            }           
+
+        }
+
+    }
+}
